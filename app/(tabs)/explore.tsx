de844a5bb29c7ae9daa7c38cef3f374b901faa2b
ProScriptSlinger <brondon.lee.dr@gmail.com>
2024-05-14 14:04:20 +0900

    feat: post and list screen update

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,102 +1,288 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
+import Ionicons from "@expo/vector-icons/AntDesign";
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Collapsible } from "@/components/Collapsible";
+import { ExternalLink } from "@/components/ExternalLink";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { FileData } from ".";
+import { Video } from "expo-av";
+
+import { Modal, TouchableOpacity } from "react-native";
+import { useEffect, useState } from "react";
+
+interface Item {
+  id: number;
+  fileUri: string;
+  name: string;
+  likes: number;
+  disLikes: number;
+  createdAt: Date;
+  views: number;
+  type: string;
+}
+
+// const fileData: Item[] = [
+//   {
+//     id: 1
+//     fileUri: "https://stirred-prawn-main.ngrok-free.app/files/3",
+//     name: "Image",
+//     likes: 2,
+//     disLikes: 3,
+//     createdAt: new Date(),
+//     views: 6,
+//     type: "image/",
+//   },
+//   {
+//     item: 2,
+//     fileUri:
+//       "file:///data/user/0/host.exp.exponent/cache/DocumentPicker/2a0f6f50-71ad-440b-8917-ade05369b9bd.mp4",
+//     name: "Video",
+//     likes: 1,
+//     disLikes: 2,
+//     createdAt: new Date(),
+//     views: 3,
+//     type: "video/",
+//   },
+// ];
 
 export default function TabTwoScreen() {
+  const [fetching, setFetching] = useState<boolean>(false);
+  const [fileData, setFileData] = useState<Item[] | []>([]);
+  // A simple functional component to render the preview based on file type
+  const FilePreview = ({ fileData }: { fileData: FileData }) => {
+    if (!fileData) return <Text>No file has been selected.</Text>;
+
+    // Check file type to decide whether to render as image or video
+    if (fileData.type.startsWith("image/")) {
+      return <Image source={{ uri: fileData.uri }} style={styles.preview} />;
+    }
+
+    if (fileData.type.startsWith("video/")) {
+      return <Video source={{ uri: fileData.uri }} style={styles.preview} />; // Assuming Video is a component for rendering videos
+    }
+
+    // Add more conditions if there are more types to support
+    return <Text>Unable to preview file type.</Text>;
+  };
+
+  const FileView = ({ fileData }: { fileData: FileData | null }) => {
+    console.log("Received Files ------->", fileData);
+    if (!fileData) return <Text>No file has been selected.</Text>;
+
+    // Check file type to decide whether to render as image or video
+    if (fileData.type.startsWith("image/")) {
+      return <Image source={{ uri: fileData.uri }} style={styles.view} />;
+    }
+
+    if (fileData.type.startsWith("video/")) {
+      return (
+        <Video
+          source={{ uri: fileData.uri }}
+          rate={1.0}
+          volume={1.0}
+          isMuted={false}
+          shouldPlay={true}
+          useNativeControls={true}
+          style={styles.view}
+        />
+      ); // Assuming Video is a component for rendering videos
+    }
+
+    // Add more conditions if there are more types to support
+    return <Text>Unable to preview file type.</Text>;
+  };
+
+  const fetchFiles = async () => {
+    console.log("Fetch files----->");
+    setFetching(true);
+    try {
+      const response = await fetch(
+        "https://stirred-prawn-main.ngrok-free.app/files",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`Server responded with status ${response.status}`);
+      }
+
+      const responseJson = await response.json();
+      setFileData(responseJson);
+      setFetching(false);
+
+      console.log("Upload successful!", responseJson);
+    } catch (error) {
+      setFetching(false);
+      console.error(
+        "Fetch failed",
+        error instanceof Error ? error.message : error
+      );
+    }
+  };
+
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [viewFile, setViewFile] = useState<FileData | null>(null);
+
+  const ListItem = (props: { file: Item }) => {
+    const { file } = props;
+    console.log("Received Files------>", file);
+    return (
+      <Collapsible
+        key={file.id}
+        title={
+          <TouchableOpacity
+            onPress={() => {
+              setModalVisible(true);
+              setViewFile({
+                name: file.name,
+                uri: `https://stirred-prawn-main.ngrok-free.app/file/${file.id}`,
+                type: file.type,
+              });
+            }}
+          >
+            <View style={styles.listItem}>
+              {/* <FilePreview
+                fileData={{
+                  name: file.name,
+                  uri: file.fileUri,
+                  type: file.type,
+                }}
+              /> */}
+              <ThemedText>{file.name}</ThemedText>
+            </View>
+          </TouchableOpacity>
+        }
+      >
+        <View style={styles.listItem}>
+          <View style={styles.listItem}>
+            <Ionicons size={18} name="like2" />
+            <ThemedText>{file.likes}</ThemedText>
+          </View>
+          <View style={styles.listItem}>
+            <Ionicons size={18} name="dislike2" />
+            <ThemedText>{file.disLikes}</ThemedText>
+          </View>
+          <View style={styles.listItem}>
+            <Ionicons size={18} name="eyeo" />
+            <ThemedText>{file.views}</ThemedText>
+          </View>
+          <View style={styles.listItem}>
+            <Ionicons size={18} name="calendar" />
+            <ThemedText>
+              {new Date(file.createdAt).toLocaleDateString()}
+            </ThemedText>
+          </View>
+        </View>
+      </Collapsible>
+    );
+  };
+
+  useEffect(() => {
+    fetchFiles();
+  }, []);
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+      headerImage={
+        <Image
+          source={require("@/assets/images/bg-2.jpg")}
+          style={styles.reactLogo}
+        />
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">Medias</ThemedText>
       </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText> library
-          to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
+      <ThemedText>You can find posted medias here.</ThemedText>
+      {fetching && <ThemedText>Loading...</ThemedText>}
+      {fileData.map((item, index) => (
+        <ListItem key={index} file={item} />
+      ))}
+      <View style={styles.container}>
+        <Modal
+          visible={modalVisible}
+          transparent={false}
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <TouchableOpacity
+            style={styles.fullScreenContainer}
+            onPress={() => setModalVisible(false)}
+          >
+            {viewFile && (
+              <FileView
+                fileData={{
+                  name: viewFile.name,
+                  uri: viewFile.uri,
+                  type: viewFile.type,
+                }}
+              />
+            )}
+          </TouchableOpacity>
+        </Modal>
+      </View>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: '#808080',
+    color: "#808080",
     bottom: -90,
     left: -35,
-    position: 'absolute',
+    position: "absolute",
   },
   titleContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 8,
+  },
+  reactLogo: {
+    height: 200,
+    width: "100%",
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+  preview: {
+    width: 50,
+    height: 50, // Set any suitable height based on your layout
+    resizeMode: "contain", // For images, maintain aspect ratio
+  },
+  view: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    resizeMode: "contain",
+  },
+  listItem: {
+    flexDirection: "row", // Lays out children in a horizontal line.
+    justifyContent: "space-between", // Distributes children evenly across container.
+    alignItems: "center",
+    gap: 6,
+  },
+  container: {
+    flex: 1,
+  },
+  thumbnail: {
+    width: 100,
+    height: 100,
+  },
+  fullScreenContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  fullScreenImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
   },
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,59 +1,171 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Button } from "react-native";
+import * as DocumentPicker from "expo-document-picker";
+import { useState } from "react";
+import { Video } from "expo-av";
+
+export interface FileData {
+  name: string;
+  type: string;
+  uri: string;
+}
 
 export default function HomeScreen() {
+  const [fileName, setFileName] = useState<string | null>();
+  const [filePreview, setFilePreview] = useState<FileData | null>(null);
+  const [uploading, setUploading] = useState<boolean>(false);
+
+  const uploadFileToServer = async (fileData: FileData): Promise<void> => {
+    setUploading(true);
+    const formData = new FormData();
+    formData.append("file", {
+      name: fileData.name,
+      type: fileData.type,
+      uri:
+        Platform.OS === "ios"
+          ? fileData.uri.replace("file://", "")
+          : fileData.uri,
+    });
+
+    try {
+      const response = await fetch(
+        "https://stirred-prawn-main.ngrok-free.app/upload/",
+        {
+          method: "POST",
+          headers: {
+            // In TypeScript, it's better to not set Content-Type manually when dealing with multipart/form-data,
+            // as the browser will set it along with the proper boundary parameter.
+            "Content-Type": "multipart/form-data",
+          },
+          body: formData,
+        }
+      );
+      console.log("response------>", response);
+      if (!response.ok) {
+        throw new Error(`Server responded with status ${response.status}`);
+      }
+
+      const responseJson = await response.json();
+      setUploading(false);
+      console.log("Upload successful");
+    } catch (error) {
+      setUploading(false);
+      console.error(
+        "Upload failed",
+        error instanceof Error ? error.message : error
+      );
+    }
+  };
+
+  // A simple functional component to render the preview based on file type
+  const FilePreview = ({ fileData }: { fileData: FileData | null }) => {
+    if (!fileData) return <Text>No file has been selected.</Text>;
+
+    // Check file type to decide whether to render as image or video
+    if (fileData.type.startsWith("image/")) {
+      return <Image source={{ uri: fileData.uri }} style={styles.preview} />;
+    }
+
+    if (fileData.type.startsWith("video/")) {
+      return <Video source={{ uri: fileData.uri }} style={styles.preview} />; // Assuming Video is a component for rendering videos
+    }
+
+    // Add more conditions if there are more types to support
+    return <Text>Unable to preview file type.</Text>;
+  };
+
+  const handleFileUpload = () => {
+    DocumentPicker.getDocumentAsync({
+      multiple: false,
+      type: "*/*", // or specify a specific mime type
+    })
+      .then((res) => {
+        // Handle the case where the user cancels the document picker.
+        if (res.canceled == true) {
+          console.log("User canceled the document picker.");
+          return;
+        }
+
+        console.log("Picked file:", res);
+
+        const pickedFileData: FileData = {
+          name: res.assets?.[0].name, // Make sure 'name' exists on the result
+          type: res.assets?.[0].mimeType || "application/octet-stream", // Use a default MIME type if mimeType is not available
+          uri: res.assets?.[0].uri,
+        };
+
+        setFileName(pickedFileData.name); // Update state with the file name
+        setFilePreview(pickedFileData); // Setting the file data for preview
+        // Now upload the file using this data
+      })
+      .catch((err) => {
+        console.log("Upload Error-----> ", err);
+      });
+  };
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/bg-1.jpg")}
           style={styles.reactLogo}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Hello!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
+        <ThemedText>At this screen, you can uplaod media files.</ThemedText>
+      </ThemedView>
+      <View style={styles.buttonContainer}>
+        <Button title="Select" onPress={() => handleFileUpload()} />
+        <Button
+          title="Post"
+          disabled={fileName != null ? false : true}
+          onPress={() => filePreview && uploadFileToServer(filePreview)}
+        />
+      </View>
+      {uploading && (
+        <View
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <ThemedText type="defaultSemiBold">Uploading...</ThemedText>
+        </View>
+      )}
+      {!uploading && fileName && (
+        <View
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+            Uploaded File: {fileName}
+          </ThemedText>
+          <FilePreview fileData={filePreview} />
+        </View>
+      )}
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -61,10 +173,22 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: 200,
+    width: "100%",
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
+  },
+  preview: {
+    width: "100%",
+    height: 300, // Set any suitable height based on your layout
+    resizeMode: "contain", // For images, maintain aspect ratio
+  },
+  button: {
+    width: "100%",
+  },
+  buttonContainer: {
+    flexDirection: "row", // Lays out children in a horizontal line.
+    justifyContent: "space-around", // Distributes children evenly across container.
   },
 });
